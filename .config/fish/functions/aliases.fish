1066668cd64b337ca50cce148c9da0539fa9298d
anthony <anthonytran.dev@gmail.com>
2020-10-26 22:19:06 +0000

    initial commit

---
+++ .config/fish/functions/aliases.fish
@@ -0,0 +1,12 @@
+# Navigation
+alias ... "cd ../../"
+alias .... "cd ../../../"
+
+# Shortcuts
+alias dl="cd ~/Downloads"
+alias dt="cd ~/Desktop"
+
+# Git
+alias ga="git add ."
+alias gm="git commit -m"
+
